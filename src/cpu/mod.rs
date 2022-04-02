33dd05a1c240cb56724996bbe1192f1442f45a42
William Banks <williamjb555@gmail.com>
2022-04-02 21:47:22 +0100

    Some cleanup + commenting

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! Parallelisation is done using the farming pattern
 
+use core::arch::x86_64::{_mm_aeskeygenassist_si128, _mm_aesenc_si128, _mm_aesenclast_si128};
+
 mod thread_pool;
 
 #[cfg(test)]
@@ -12,14 +14,16 @@ fn test_aes_encrypt() {
 
 /// Perform AES-128/CTR encryption on slice `data` using slice `key` (`key` having gone through necessary key derivation and being exactly 128-bit)
 /// # Panics
-/// This function will panic if used on an architecture that is not x86/x86_64 and when AES-NI isn't available
+/// This function will panic if `key` is not 128-bit or if used on an architecture that is not x86/x86_64 or when AES-NI isn't available
 pub fn aes_encrypt<'a>(data: &'a mut [u8], key: &[u8]) -> &'a mut [u8] {
+	assert_eq!(key.len(), 16);
+
 	// Compile the block if the target architecture is x86 or x86_64
 	#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 	{
 		// Check for AES-NI
 		if is_x86_feature_detected!("aes") {
-			// TODO: Actually perform AES encryption
+			// TODO: Splitting `data` up into 128-bit blocks, generating the counter for each block, and giving the work to a ThreadPool instance to work on
 
 			return data;
 		}
@@ -29,19 +33,32 @@ pub fn aes_encrypt<'a>(data: &'a mut [u8], key: &[u8]) -> &'a mut [u8] {
 	unimplemented!();
 }
 
-/// Performs the cipher on a 128-bit state with 11 128-bit round keys
+/// Expands one 128-bit key into 11 128-bit round keys
 /// # Panics
-/// This function panics if the state slice length is not equal to 16, and/or the round keys slice length is not equal to 176 (16 * 11)
-fn do_cipher<'a>(state: &'a mut [u8], round_keys: &[u8]) -> &'a mut [u8] {
-	assert_eq!(state.len(), 16, "`state` length not equal to 16");
-	assert_eq!(round_keys.len(), 176, "`round_keys` length not equal to 176"); // 16 * 11
+/// This function panics if `key` is not 128-bit
+fn key_expansion(key: &[u8]) -> [u8; 176] {
+	assert_eq!(key.len(), 16);
+
+	// Mutable array to work on
+	let mut rks = [0u8; 176];
+
+	// TODO: Key expansion using `_mm_aeskeygenassist_si128` intrinsic
 
 	todo!();
 }
 
+/// Performs the cipher on a 128-bit state with 11 128-bit round keys
+/// # Panics
+/// This function panics if the `state` length is not equal to 16, and/or `round_keys` length is not equal to 176 (16 * 11)
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-// Enable AES-NI for this function. This will compile code using AES-NI even if the CPU doesn't support it, so we have to check at runtime whether the CPU supports it before calling this function
+// Enable AES-NI for this function
+// This will compile code using AES-NI even if the CPU doesn't support it, so we have to check at runtime whether the CPU supports it before calling this function
 #[target_feature(enable = "aes")]
-unsafe fn do_round<'a>(state: &'a mut [u8], round_key: &[u8]) -> &'a mut [u8] {
+unsafe fn cipher<'a>(state: &'a mut [u8], round_keys: &[u8]) -> &'a mut [u8] {
+	assert_eq!(state.len(), 16);
+	assert_eq!(round_keys.len(), 176); // 16 * 11
+
+	// TODO: Cipher using `_mm_aesenc_si128` and `_mm_aesenclast_si128` intrinsics
+
 	todo!();
 }
--- src/cpu/thread_pool.rs
+++ src/cpu/thread_pool.rs
@@ -1,4 +1,4 @@
-//! This module implements a thread pool with a number of threads returned from std::thread::available_parallelism if Some, or if None, then 4
+//! This module implements a thread pool
 
 use std::{sync::{mpsc, Arc, Mutex, atomic::{AtomicUsize, Ordering}, Condvar}, thread};
 
@@ -32,26 +32,28 @@ impl<F: FnOnce()> FnOnceBox for F {
 	}
 }
 
+/// A trait type of a Box (unique pointer) around a function that needs to be called only once (using non-callable trait object workaround) and that is safe to copy/pass between threads and that lives as long as the entire program
 type Task = Box<dyn FnOnceBox + Send + 'static>;
 
+/// Message enum for passing to worker threads. 2 variants: NewTask and Terminate
 enum Message {
 	NewTask(Task),
 	Terminate
 }
 
+/// Convenience wrapper around a thread
 struct Worker {
 	thread: Option<thread::JoinHandle<()>>
 }
 
 impl Worker {
+	/// Creates a thread that waits for messages and acts appropriately upon reception of them. The thread also decrements `num_tasks` when a task is completed and notifies `await_condvar`'s Condvar
 	fn new(reciever: Arc<Mutex<mpsc::Receiver<Message>>>, await_condvar: Arc<(Condvar, Mutex<()>)>, num_tasks: Arc<AtomicUsize>) -> Self {
 		Worker {
 			thread: Some(thread::spawn(move || {
 				loop {
+					// Recieve a message from the channel (blocking when there are none available)
 					let message = reciever.lock().unwrap().recv().unwrap();
-
-					num_tasks.fetch_sub(1, Ordering::SeqCst);
-					await_condvar.0.notify_one();
 
 					match message {
 						Message::NewTask(taskptr) => {
@@ -59,6 +61,10 @@ impl Worker {
 						},
 						Message::Terminate => break
 					}
+
+					// Decrement `num_tasks` and notify the condition variable
+					num_tasks.fetch_sub(1, Ordering::SeqCst);
+					await_condvar.0.notify_one();
 				}
 			}))
 		}
@@ -73,23 +79,28 @@ pub struct ThreadPool {
 }
 
 impl ThreadPool {
+	/// Construct a ThreadPool with a number of worker threads equal to the return value of `std::thread::available_parallelism` if `Some`, or if `None`, then 4
 	pub fn new() -> Self {
 		let num_workers = match thread::available_parallelism() {
 			Ok(n) => n.into(),
 			Err(_) => 4
 		};
 
+		// Create a multiple producer single consumer channel
 		let (sender, reciever) = mpsc::channel();
 
 		// Protect the reciever for using across threads
 		let reciever = Arc::new(Mutex::new(reciever));
 
+		// Preallocate
 		let mut workers = Vec::with_capacity(num_workers);
 
 		// Use an atomic usize for keeping track of number of tasks
 		let num_tasks = Arc::new(AtomicUsize::new(0));
+		// Create a Condvar and an associated Mutex that will be used for the `await_all` function
 		let await_condvar = Arc::new((Condvar::new(), Mutex::new(())));
 
+		// Create the workers
 		for _ in 0..num_workers {
 			workers.push(Worker::new(Arc::clone(&reciever), Arc::clone(&await_condvar), Arc::clone(&num_tasks)));
 		}
@@ -102,12 +113,15 @@ impl ThreadPool {
 		}
 	}
 
+	/// Assign a task to the ThreadPool that will be executed by a thread at some indeterminate point in the future
 	pub fn assign_task<F>(&self, function: F) where F: FnOnce() + Send + 'static {
 		let message = Message::NewTask(Box::new(function));
 		self.sender.send(message).unwrap();
+		// Increment the assigned tasks counter
 		self.num_tasks.fetch_add(1, Ordering::SeqCst);
 	}
 
+	/// Blocks until all currently assigned tasks are complete
 	pub fn await_all(&self) {
 		// Wait until the number of tasks left is 0
 		let _ = self.await_condvar.0.wait_while(self.await_condvar.1.lock().unwrap(), |_| {
@@ -118,6 +132,7 @@ impl ThreadPool {
 }
 
 impl Drop for ThreadPool {
+	/// Define behaviour for when ThreadPool goes out of scope/is dropped - We want to shut down all threads gracefully
 	fn drop(&mut self) {
 		// Instruct all workers to finish
 		for _ in 0..self.workers.len() {
